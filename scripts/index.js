5baf4b972519ee9f194a39bf53306a243633691f
Murphy <murphychampion@gmail.com>
2017-07-12 18:14:04 -0500

    changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,15 @@
-$(document).ready(function () {
-                  $('#mugshot').fadeIn();
-                  
-                  setTimeout(function () {
-                             $('#name').fadeIn();
-                             
-                             setTimeout(function () {
-                                        $('#info').fadeIn();
-                                        }, 200);
-                             }, 200);
+//$(document).ready(function () {
+//                  $('#mugshot').fadeIn();
+//                  
+//                  setTimeout(function () {
+//                             $('#name').fadeIn();
+//                             
+//                             setTimeout(function () {
+//                                        $('#info').fadeIn();
+//                                        }, 200);
+//                             }, 200);
+//});
+
+$('.button').on('click', function () {
+    alert('clicked button');
 });
