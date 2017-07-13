40c1888badc47a3bb65405ec001fd788c06ae090
Murphy <murphychampion@gmail.com>
2017-07-13 07:46:44 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,14 @@
-$(window).load(function () {
-   $('#mugshot').fadeIn();
+$(document).ready(function () {
+   $(window).load(function () {
+      $('#mugshot').fadeIn();
    
-   setTimeout(function () {
-      $('#name').fadeIn();
-      
       setTimeout(function () {
-         $('#info').fadeIn();
-         $('#info2').fadeIn();
+         $('#name').fadeIn();
+      
+         setTimeout(function () {
+            $('#info').fadeIn();
+            $('#info2').fadeIn();
+         }, 200);
       }, 200);
-   }, 200);
+   });
 });
