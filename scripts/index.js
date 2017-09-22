f0d61de24f7f48e69130d876f14ce6a8c6434852
Murphy <murphychampion@gmail.com>
2017-09-22 14:11:46 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,7 @@
 $(document).ready(function () {
     $('#name').fadeIn(1500, "linear", function () {
-        $('#position').fadeIn(1000, "linear", function () {
-            $('#location').fadeIn(1000, "linear", function () {
-                setTimeout(function () {
-                    $('#blurb').fadeIn(1000, "linear", function () {
-                        setTimeout(function() { $('.buttons').fadeIn(750); }, 500);
-                    });
-                }, 450);
+        $('#summary').fadeIn(1000, "linear", function () {
+                setTimeout(function() { $('.buttons').fadeIn(750); }, 500);                                    
             });
         });
     });
